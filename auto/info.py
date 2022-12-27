0de82b402c967811c1d042feb252b4f8f7b17847
Sergey Krashevich <svk@svk.su>
2022-12-27 16:34:45 +0300

    Revert "SD 2.1 fixes"
    This reverts commit f014012bd33b756ecbf6969eda97d323c8d0bc75.

--- auto/info.py
+++ auto/info.py
@@ -7,7 +7,7 @@ file.write_text(
   .replace('    return demo', """
     with demo:
         gr.Markdown(
-          'Created by [AUTOMATIC1111 / stable-diffusion-webui-docker](https://github.com/skrashevich/stable-diffusion-webui)'
+          'Created by [AUTOMATIC1111 / stable-diffusion-webui-docker](https://github.com/AbdBarho/stable-diffusion-webui-docker/)'
         )
     return demo
 """, 1)
