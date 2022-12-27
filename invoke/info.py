f014012bd33b756ecbf6969eda97d323c8d0bc75
Sergey Krashevich <svk@svk.su>
2022-12-27 16:06:56 +0300

    SD 2.1 fixes

--- invoke/info.py
+++ invoke/info.py
@@ -7,7 +7,7 @@ file.write_text(
   .replace('  <div id="root"></div>', """
     <div id="root"></div>
     <div>
-      Deployed with <a href="https://github.com/AbdBarho/stable-diffusion-webui-docker/">stable-diffusion-webui-docker</a>
+      Deployed with <a href="https://github.com/skrashevich/stable-diffusion-webui">stable-diffusion-webui-docker</a>
     </div>
 """, 1)
 )
